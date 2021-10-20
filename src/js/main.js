edd4763d4dfa9b32f0093ffaabe3501dae93d3ed
sergokiko <sergokiko@gmail.com>
2021-10-20 12:00:47 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import '../scss/main.scss'
+
+const a = 1;
+
+console.log(a);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ]
+            },
+        ]
+    },
+
+    mode: 'development',
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'src/index.html'
+    })],
+    devServer: {
+        port: 8080
+    }
+};
